0f52d70130fe4be04dd35485aae4a0b34118f547
Daisy Muyldermans <daisy.muyldermans@hotmail.com>
2019-03-06 09:49:32 +0000

    basic HTML and CSS

---
+++ main.js
@@ -0,0 +1,49 @@
+'use strict';
+
+//require
+var express = require("express");
+
+
+var port = 8081;
+
+var app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "templates");
+app.use(express.static("static"));
+
+
+//HOMEPAGE
+app.get('/', function(req, res) {
+    res.render('index.ejs');
+});
+
+//ABOUT
+app.get('/about', function(req, res) {
+    res.render('about');
+});
+
+//CONTACT
+app.get('/contact', function(req, res) {
+    res.render('contact');
+});
+
+//NEWS
+app.get('/news', function(req, res) {
+    res.render('news');
+});
+
+//RESOURCES
+app.get('/resources', function(req, res) {
+    res.render('resources');
+});
+
+
+
+
+//set the server to listen on a port
+app.listen(port);
+
+
+console.log("server running http://localhost:" + port);
+
